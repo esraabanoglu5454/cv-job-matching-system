31b17c8af11ba5f1aa897f12f8d47757b604c4ee
esraabanoglu5454 <esra54abanoglu@gmail.com>
2026-05-26 23:11:10 +0300

    Remove email scope from Facebook login

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -250,7 +250,7 @@ SOCIALACCOUNT_PROVIDERS = {
     },
     "facebook": {
         "METHOD": "oauth2",
-        "SCOPE": ["email", "public_profile"],
+        "SCOPE": ["public_profile"],
         "AUTH_PARAMS": {
             "auth_type": "reauthenticate",
         },
